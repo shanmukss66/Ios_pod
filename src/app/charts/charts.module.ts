fbe8c3145f0b3e072e92799e3f6d45cda4f8b4b2
shanmukss66 <shanmuka16@gmail.com>
2020-07-30 11:30:52 +0530

    updated

--- src/app/charts/charts.module.ts
+++ src/app/charts/charts.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
-
+import {FlexLayoutModule} from '@angular/flex-layout';
 import { ChartsPageRoutingModule } from './charts-routing.module';
 import {ChartsModule} from 'ng2-charts';
 import { ChartsPage } from './charts.page';
@@ -14,6 +14,7 @@ import { ChartsPage } from './charts.page';
     FormsModule,
     IonicModule,
     ChartsModule,
+    FlexLayoutModule,
     ChartsPageRoutingModule,
     
   ],
